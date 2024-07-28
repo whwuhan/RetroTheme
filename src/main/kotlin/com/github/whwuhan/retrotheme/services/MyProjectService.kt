6ff790d5be9ee00c9bc91c2ab5ee7e8a30107bb4
GitHub Action <action@github.com>
2024-07-28 07:19:10 +0000

    Template cleanup

--- src/main/kotlin/com/github/whwuhan/retrotheme/services/MyProjectService.kt
+++ src/main/kotlin/com/github/whwuhan/retrotheme/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.whwuhan.retrotheme.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.whwuhan.retrotheme.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
